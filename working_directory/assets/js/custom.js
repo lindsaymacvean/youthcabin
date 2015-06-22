5c90aa29305797cb8a2829cd38ed486735d12913
Lindsay <lindsaymacvean@gmail.com>
2015-06-22 23:32:44 +0100

    moved email script to custom.js to make it more modular

--- working_directory/assets/js/custom.js
+++ working_directory/assets/js/custom.js
@@ -24,70 +24,28 @@
 
 
 jQuery(document).ready(function($) {
-      /*
-      // Create the dropdown base
-      $("<select />").appendTo("nav");
-      
-      // Create default option "Go to..."
-      $("<option />", {
-         "selected": "selected",
-         "value"   : "",
-         "text"    : "Go to..."
-      }).appendTo("nav select");
-      
-      // Populate dropdown with menu items
-      $("nav a").each(function() {
-       var el = $(this);
-       $("<option />", {
-           "value"   : el.attr("href"),
-           "text"    : el.text()
-       }).appendTo("nav select");
-      });
-      
-      
-	   // To make dropdown actually work
-	   // To make more unobtrusive: http://css-tricks.com/4064-unobtrusive-page-changer/
-      $("nav select").change(function() {
-        window.location = $(this).find("option:selected").val();
-      });
-
-				$("a[data-pretty^='prettyPhoto']").prettyPhoto();
 				
-				$(".gallery:first a[data-pretty^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'pp_default',slideshow:3000, autoplay_slideshow: false});
-				$(".gallery:gt(0) a[data-pretty^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
-		
-				$("#custom_content a[data-pretty^='prettyPhoto']:first").prettyPhoto({
-					custom_markup: '<div id="map_canvas" style="width:260px; height:265px"></div>',
-					changepicturecallback: function(){ initialize(); }
-				});
+	//Fix Youtube
+  $('iframe').each(function () {/*fix youtube z-index*/
+      var url = $(this).attr("src");
+      if (url.indexOf("youtube.com") >= 0) {
+          if (url.indexOf("?") >= 0) {
+              $(this).attr("src", url + "&wmode=transparent");
+          } else {
+              $(this).attr("src", url + "?wmode=transparent");
+          }
+      }
+  });
 
-				$("#custom_content a[data-pretty^='prettyPhoto']:last").prettyPhoto({
-					custom_markup: '<div id="bsap_1259344" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div><div id="bsap_1237859" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6" style="height:260px"></div><div id="bsap_1251710" class="bsarocks bsap_d49a0984d0f377271ccbf01a33f2b6d6"></div>',
-					changepicturecallback: function(){ _bsap.exec(); }
-				});
-  //*/
-				
-				
-    $('iframe').each(function () {/*fix youtube z-index*/
-        var url = $(this).attr("src");
-        if (url.indexOf("youtube.com") >= 0) {
-            if (url.indexOf("?") >= 0) {
-                $(this).attr("src", url + "&wmode=transparent");
-            } else {
-                $(this).attr("src", url + "?wmode=transparent");
-            }
-        }
-    });
+  $('ul.nav li.dropdown').hover(function () {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+  }, function () {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+  });
 
-    $('ul.nav li.dropdown').hover(function () {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
-    }, function () {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-    });
-	
-    // tooltip
-    $("a[data-rel^='tooltip']").tooltip();
-    $('.tooltip').tooltip();
+  // tooltip
+  $("a[data-rel^='tooltip']").tooltip();
+  $('.tooltip').tooltip();
 	
   $('#mainslider').flexslider({
     animation: "slide",
@@ -95,7 +53,48 @@ jQuery(document).ready(function($) {
   });
 
 
-  // Contact Form Script
+  // Email Newsletter Script
+  $(function() {
+    
+    // Get the form.
+    var form = $('#mail-subscribe-div>form');
+
+    //Event Listener
+    $(form).submit(function(e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
+
+       // Serialize the form data.
+      var formData = $(form).serialize();
+      
+      //Ajax reques
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      })
+      .done(function (result) {
+          var data = JSON.parse(result);
+          console.log(data);
+          $("#mail-subscribe-div")
+                 .html ("Thanks, you have been added to our mailing list!"
+                           + "<br><br><br>");
+          return false;
+        })
+      .fail(function(result){
+          var data = JSON.parse(result);
+          console.log(data);
+          $("#mail-subscribe-error")
+                   .text ("Sorry, there is something wrong with that e-mail "
+                             + "address, please try again.");
+      });
+      return false;
+       //*/
+    });
+   
+
+  });
+   
 
   $(function() {
 
